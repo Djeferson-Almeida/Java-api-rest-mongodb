e764ca3f6e9cdac62c7880b2e8873138f916a5fd
Djeferson Almeida <djefersonalmeida20@gmail.com>
2024-03-27 13:38:39 -0300

    Projeto Spring 1.8 - Insert method with URI implemented!

--- projectweb/src/main/java/com/projetospring/projectweb/resources/UserResource.java
+++ projectweb/src/main/java/com/projetospring/projectweb/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.projetospring.projectweb.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projetospring.projectweb.entities.User;
 import com.projetospring.projectweb.services.UserService;
@@ -29,6 +33,15 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	//Insert Method completeted with URI
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri =ServletUriComponentsBuilder.fromCurrentRequest().path("/{Id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 		
 }
