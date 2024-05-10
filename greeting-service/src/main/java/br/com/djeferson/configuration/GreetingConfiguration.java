5ec9d4a8f2178ebf88eb9ba4180d542f2df58bd8
Djeferson Almeida <djefersonalmeida20@gmail.com>
2024-05-10 07:45:34 -0300

    Projeto Spring Cloud sem cloud 1.1 - projeto finalizado

--- greeting-service/src/main/java/br/com/djeferson/configuration/GreetingConfiguration.java
+++ greeting-service/src/main/java/br/com/djeferson/configuration/GreetingConfiguration.java
@@ -4,7 +4,31 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties
+@ConfigurationProperties("greeting-service")
 public class GreetingConfiguration {
 
+	private String greeting;
+	private String defaultValue;
+	
+	public GreetingConfiguration() {
+	}
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+
+	public void setDefaultValue(String defaultValue) {
+		this.defaultValue = defaultValue;
+	}
+	
+	
+	
 }
