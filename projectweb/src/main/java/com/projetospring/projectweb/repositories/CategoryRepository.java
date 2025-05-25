e635d4aab4dc935da3b492124da961216739bcbd
Djeferson Almeida <djefersonalmeida20@gmail.com>
2025-05-25 14:13:39 -0300

    Adding a project

--- projectweb/src/main/java/com/projetospring/projectweb/repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.projetospring.projectweb.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projetospring.projectweb.entities.Category;
-
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-	
-}
